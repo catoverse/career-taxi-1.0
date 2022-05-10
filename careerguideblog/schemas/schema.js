c919c9874ad2f644271302c1a312ab6ff01de46a
ashwindevx <ashwindevx@gmail.com>
2022-05-10 13:14:34 +0530

    updated with master branch commits

--- careerguideblog/schemas/schema.js
+++ careerguideblog/schemas/schema.js
@@ -10,6 +10,7 @@ import category from "./category";
 import post from "./post";
 import author from "./author";
 import richText from "./richText";
+import mux from "./mux";
 // import youtube from "./youtube";
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -28,6 +29,7 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
+    mux,
     // youtube,
   ]),
 });
