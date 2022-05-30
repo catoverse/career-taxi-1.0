569f9648694234e7f8c936cca5e95f114cb0d7ce
gauravchauhan2403 <gauravchauhan2403@gmail.com>
2022-05-30 16:01:36 +0530

    added result calculation logic and page

--- components/assessment/assessment-radio.js
+++ components/assessment/assessment-radio.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Formik, Field, FieldArray, Form } from "formik";
-
+import React from "react"; 
+import { Formik, Field, FieldArray, Form , useFormik} from "formik";
 // put carousel inside single form component
 // field array for picked value
 // get all the values on last submit
+// const formik = useFormik()
 
 const AssessmentRadio = ({
   question,
@@ -15,18 +15,21 @@ const AssessmentRadio = ({
   const handleChange = (e) => {
     field.onChange(e);
     if (!isLastSlide) setCurrentSlide((currentSlide) => currentSlide + 1);
+    console.log(e.target.value);
+    console.log(question);
   };
+
 
   return (
     <div className="text-left">
-      <p className="mb-4 text-headFour font-bold">{question}</p>
+      <p className="mb-4 text-headFour font-bold">{question.Question}</p>
       <div className="mb-2">
         <label className="cursor-pointer">
           <input
             type="radio"
             name={field.name}
             onChange={handleChange}
-            value="1"
+            value={question.Reverse=== 'Yes' ? 5:1}
             className="mr-3 h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-300 bg-white transition duration-300 checked:border-[5px] checked:border-yellow"
           />
           Strongly Disagree
@@ -38,7 +41,7 @@ const AssessmentRadio = ({
             type="radio"
             name={field.name}
             onChange={handleChange}
-            value="2"
+            value={question.Reverse=== 'Yes' ? 4:2}
             className="mr-3 h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-300 bg-white transition duration-300 checked:border-[5px] checked:border-yellow"
           />
           Disagree
@@ -62,7 +65,7 @@ const AssessmentRadio = ({
             type="radio"
             name={field.name}
             onChange={handleChange}
-            value="4"
+            value={question.Reverse=== 'Yes' ? 2:4}
             className="mr-3 h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-300 bg-white transition duration-300 checked:border-[5px] checked:border-yellow"
           />
           Agree
@@ -74,7 +77,7 @@ const AssessmentRadio = ({
             type="radio"
             name={field.name}
             onChange={handleChange}
-            value="5"
+            value={question.Reverse=== 'Yes' ? 1:5}
             className="mr-3 h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-300 bg-white transition duration-300 checked:border-[5px] checked:border-yellow"
           />
           Strongly Agree
---
+++ pages/assessment/assessment-result.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "../../components/commons/header.js";
+import Footer from "../../components/commons/footer.js";
+import { useRouter } from 'next/router'
+import { route } from "next/dist/server/router";
+
+
+const AssessmentResult = () => {
+
+    const router = useRouter()
+    const [answers,setAnswers] = React.useState([]);
+    React.useEffect(()=>{
+       if (router.isReady) 
+       setAnswers(router.query.answers)
+    },[router.isReady])
+    // console.log('query',router.query.answers)
+    let numbers = answers?.map((n) => {
+      return parseInt(n, 10);
+    });
+    console.log('number',typeof(numbers));
+    let sum =0;
+    for(let num of numbers){
+      sum+=num;
+    }
+    console.log(sum);
+   let percentage =((sum/120)*100).toFixed(2); 
+   let average = (sum/24).toFixed(2);
+  return(
+  <>
+    <Header />
+    <div className="flex place-content-center">
+    <div className="m-2 px-40 py-20 flex-col border-4 border-black border-solid rounded">
+      <p className="text-8xl font-extrabold leading-8 border-b border-black border-solid">Result Card</p>
+      <p className="text-8xl font-extrabold leading-8">Percentage scored</p>
+      <div className="text-2xl font-extrabold leading-7">{percentage}%</div>
+      <p className="text-8xl font-extrabold leading-8">Average Score</p>
+      <div className="text-2xl font-extrabold leading-7">{average}</div>
+    </div>
+    </div>
+    <Footer />
+  </>
+  );
+}
+
+export default AssessmentResult;
